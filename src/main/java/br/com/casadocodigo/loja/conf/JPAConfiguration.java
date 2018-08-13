151d87fe1eadbb906e55f5a9317d0eefc62c9d10
Paulo Sérgio <pauloqueiroz16@gmail.com>
2018-08-12 22:27:07 -0300

    Configuração de gerenciador de transações

--- src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -2,8 +2,11 @@ package br.com.casadocodigo.loja.conf;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -35,5 +38,10 @@ public class JPAConfiguration {
         factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models");
 
         return factoryBean;
+    }
+	
+	@Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
     }
 }
