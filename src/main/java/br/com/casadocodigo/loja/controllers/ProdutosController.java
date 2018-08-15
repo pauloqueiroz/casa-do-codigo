d3f0f14dbe6e4d85f4512e578a14d087bee251f6
Paulo Sergio <paulosergio@infoway-pi.com.br>
2018-08-15 18:00:44 -0300

    configuração de encoding e listagem de produtos

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
@@ -10,6 +13,7 @@ import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
+@RequestMapping("produtos")
 public class ProdutosController {
 	
 	@Autowired
@@ -20,14 +24,14 @@ public class ProdutosController {
 //		return "produtos/form";
 //	}
 
-	@RequestMapping("/produtos")
+	@RequestMapping(method=RequestMethod.POST)
 	public String grava(Produto produto) {
 		System.out.println(produto);
 		produtoDao.gravar(produto);
 		return "produtos/sucesso";
 	}
 	
-	@RequestMapping("/produtos/form")
+	@RequestMapping("form")
     public ModelAndView form(){
 
         ModelAndView modelAndView = new ModelAndView("produtos/form");
@@ -35,4 +39,11 @@ public class ProdutosController {
 
         return modelAndView;
     }
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView listar(){
+	    List<Produto> produtos = produtoDao.listar();
+	    ModelAndView modelAndView = new ModelAndView("/produtos/lista");
+	    modelAndView.addObject("produtos", produtos);
+	    return modelAndView;
+	}
 }
