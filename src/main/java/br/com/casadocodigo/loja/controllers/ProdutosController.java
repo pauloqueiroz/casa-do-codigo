5f2c5d59959540b0ede3496fa8435169245d2b65
Paulo Sérgio <pauloqueiroz16@gmail.com>
2018-08-16 19:50:50 -0300

    implementação da validação de produtos através do Validator do Spring

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validations.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -24,9 +30,17 @@ public class ProdutosController {
 //	public String form() {
 //		return "produtos/form";
 //	}
+	
+	@InitBinder
+	public void InitBinder(WebDataBinder binder){
+	    binder.addValidators(new ProdutoValidation());
+	}
 
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView grava(Produto produto, RedirectAttributes redirectAttributes) {
+	public ModelAndView grava(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()){
+	        return form();
+	    }
 		System.out.println(produto);
 	    produtoDao.gravar(produto);
 	    redirectAttributes.addFlashAttribute("sucesso","Produto cadastrado com sucesso!");
